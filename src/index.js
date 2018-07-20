6bf84ad5d89b30c59e2cac76a34d16ea57ff0c41
Andrew <andrew.liu01@libertymutual.com>
2018-07-20 18:41:46 -0400

    Enable flow on index.js and resolve problem

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+if (root === null) {
+  throw new Error('Root element not found.');
+}
+ReactDOM.render(<App />, root);
 registerServiceWorker();
