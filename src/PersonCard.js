e8ce5ec99dffdf109b590b4dfe68d67d84051010
andr3w <liu.anray+github@gmail.com>
2018-07-21 21:42:50 -0400

    Implement multiline text field

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -1,41 +1,62 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 const styles = {
   card: {
     maxWidth: 480
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%' // 16:9
   }
 };
 
-function PersonCard(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
-            Sherlock H.
-          </Typography>
-          <Typography component="p">Text editor goes here.</Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="secondary">
-            Delete
-          </Button>
-        </CardActions>
-      </Card>
-    </div>
-  );
+type Props = {};
+
+type State = {
+  textFieldValue: string
+};
+
+class PersonCard extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textFieldValue: ''
+    };
+    (this: any).updateTextValue = this.updateTextValue.bind(this);
+  }
+
+  updateTextValue(event) {
+    this.setState({ textFieldValue: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              Sherlock H.
+            </Typography>
+            <TextField
+              label="Notes"
+              value={this.state.textFieldValue}
+              onChange={this.updateTextValue}
+              multiline={true}
+            />
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="secondary">
+              Delete
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(PersonCard);
